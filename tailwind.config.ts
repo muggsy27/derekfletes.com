17c5ddb9a8cb1fe8fea1760553b78857e8e73b36
Derek Fletes <derekfletes@gmail.com>
2023-10-07 01:43:01 -0700

    Added container properties

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,jsx}"],
-  theme: {},
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
   plugins: [require("daisyui")],
 } satisfies Config;
